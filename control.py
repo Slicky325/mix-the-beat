17a299c12742399039efa1d5cb13a94159bbf09b
Slicky325 <Vansh3022005@outlook.com>
2024-01-16 01:49:59 +0530

    minor changes

--- control.py
+++ control.py
@@ -1,8 +1,7 @@
 from flask import Flask     
 app = Flask(__name__)   # Flask constructor 
   
-# A decorator used to tell the application 
-# which URL is associated function 
+# A decorator used to tell the application which URL is associated function 
 @app.route('/')       
 def hello(): 
     return 'HELLO'
